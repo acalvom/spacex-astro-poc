1c4111b93eb1858c43cb41645f84f13aece57ae9
acalvom <andreac.3108@gmail.com>
2024-02-13 17:31:10 +0100

    build: add eslint & config prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['plugin:astro/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {}
+    }
+  ]
+}
