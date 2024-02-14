692d060c02a542820bb04c511389b64eee35902d
acalvom <andreac.3108@gmail.com>
2024-02-14 10:03:44 +0100

    style: add breakpoints to theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,13 @@ export default {
           900: '#134e4a',
           950: '#042f2e'
         }
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
       }
     }
   },
